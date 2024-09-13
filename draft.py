f5c4d790a3087bc1953ff122eb8e2a8c53ceab32
Jorge Luís Torres <jorgetorresjunior175@gmail.com>
2024-09-13 00:12:32 +0000

    feat: created a function to insert rows

--- draft.py
+++ draft.py
@@ -1,34 +1,21 @@
-import pandas as pd
-import sqlite3
+from functions.insert_rows import insert_one_row
+from functions.create_table import create_table
 
-# create a data example
-data = pd.DataFrame({
-    'ID': [1, 2, 3, 4],
-    'Name': ['Alice', 'Bob', 'Charlie', 'David']
-})
 
-# create a connection
-conn = sqlite3.connect('mydatabase.db')
-
-# Use the to_sql method to save the DataFrame to a table in the database
-data.to_sql(
-    'client', conn,
-    if_exists='replace',
-    index=False
+create_table(
+    database='mydatabase',
+    table_name='produtos',
+    columns_desc="""
+        id_produto INTEGER PRIMARY KEY,
+        nome CHAR NOT NULL,
+        qtd INTEGER NOT NULL
+    """
 )
 
-# closing the connection
-conn.close()
+insert_one_row(
+   database_name='mydatabase',
+    table_name='produtos',
+    columns_name='nome, qtd',
+    values="'Xbox', 90"
+)
 
-
-########## 
-conn = sqlite3.connect('mydatabase.db')
-
-query = """ 
-    SELECT Name 
-    FROM client
-    WHERE name IN ('Alice', 'David')
-"""
-
-
-pd.read_sql_query(query, conn)
